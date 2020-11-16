2d0f69d08c1828ec81c78e3eb9e8e99f68f3a0d2
KlasJoensson <klas.joensson@gmail.com>
2020-11-16 14:15:43 +0100

    updates the controller class

--- greetings/src/main/java/com/example/handlingformsubmission/GreetingController.java
+++ greetings/src/main/java/com/example/handlingformsubmission/GreetingController.java
@@ -1,9 +1,37 @@
 package com.example.handlingformsubmission;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * A Java class that represents the controller for this application.
- * TODO Implement
  */
+@Controller
 public class GreetingController {
+	@Autowired
+	private DynamoDBEnhanced dde;
 
+	@Autowired
+	private PublishTextSMS msg;
+
+	@GetMapping("/")
+	public String greetingForm(Model model) {
+		model.addAttribute("greeting", new Greeting());
+		
+		return "greeting";
+	}
+	
+	@PostMapping("/greeting")
+	public String greetingSubmit(@ModelAttribute Greeting greeting) {
+	      //Persist submitted data into a DynamoDB table using the enhanced client
+	      dde.injectDynamoItem(greeting);
+	      // Send a mobile notification
+	      msg.sendMessage(greeting.getId());
+
+	      return "result";
+	}
 }
