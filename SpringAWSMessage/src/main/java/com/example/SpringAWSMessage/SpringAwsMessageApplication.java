a754c154a7f220c0e94fcf3e3ebc131227522caa
KlasJoensson <klas.joensson@gmail.com>
2020-11-19 09:31:18 +0100

    Init project

---
+++ SpringAWSMessage/src/main/java/com/example/SpringAWSMessage/SpringAwsMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringAWSMessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsMessageApplication.class, args);
+	}
+
+}
